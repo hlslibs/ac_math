a99a55404316cd3662d4499eea8019c1e8dd883b
Nikolaos Kavvadias <kavvadias@silexica.com>
2021-04-20 19:12:12 +0200

    rtest_ac_exp_pwl.cpp: Report old/actual when both available

--- tests/rtest_ac_exp_pwl.cpp
+++ tests/rtest_ac_exp_pwl.cpp
@@ -140,8 +140,6 @@ int test_driver(
 
       // Update the old value
       old_output_exp = actual_value_exp;
-      // Once an old value has been stored, i.e. towards the end of the first iteration, this value is set to true.
-      compare_exp = true;
 
       // same thing as above, but for the natural exponential.
       if (compare_exp) {
@@ -153,6 +151,7 @@ int test_driver(
           assert(false); // LCOV_EXCL_LINE
         }
       }
+      // Once an old value has been stored, i.e. towards the end of the first iteration, this value is set to true.
       old_output_exp = actual_value_exp;
       compare_exp = true;
     }
